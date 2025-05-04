032557b479329a2475ec653e6fd479349dec77fa
bourne again pr5 <137082411+bapr5@users.noreply.github.com>
2025-05-04 17:49:42 +0300

    modified:   app/file-index.mjs

--- app/file-index.mjs
+++ app/file-index.mjs
@@ -1,20 +1,22 @@
-import fs from "fs";
+import fs from "fs/promises";
 
 async function buildFileIndex() {
   let fileIndex = [];
-  fs.readdir("./movies", (err, files) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      return;
-    }
+  try {
+    const files = await fs.readdir("./movies");
     files.forEach((file) => {
+      console.log("File:", file);
       let link = encodeURI(`/movies/${file}`);
-      let newFileEntry = {name:file,
-        link:link,
+      let newFileEntry = {
+        name: file,
+        link: link,
       };
       fileIndex.push(newFileEntry);
     });
-  });
+  } catch (err) {
+    console.error("Error reading directory:", err);
+  }
+  console.log("File index:", fileIndex);
   return fileIndex;
 }
 
